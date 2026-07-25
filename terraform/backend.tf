fa4e84d5921076ec395b3532f69a035e2809ce36
Partho M <parthomazumdar999@gmail.com>
2026-07-25 16:34:42 +0530

    placed the s3 bucket file in backend.tf again 3

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.7.0"
 
   backend "s3" {
-    bucket       = "part--aps1-az1--x-s31--aps1-az1--x-s3"   # ← Change this to a real bucket name
+    bucket       = "part--aps1-az1--x-s31--aps1-az1--x-s3--aps1-az1--x-s3"   # ← Change this to a real bucket name
     key          = "ecommerce/prod/terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
